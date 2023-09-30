627436f5d4556c3a1119887693c4ac2a447161f7
Sven Walter <NightmareWolfGamesGermany@web.de>
2023-09-30 23:22:28 +0200

    Second Commit
    Open the New C++ Class in Rider

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// copyright  Nightmare Wolf Games Germany
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// copyright  Nightmare Wolf Games Germany
+
+#pragma once
+
+#include "CoreMinimal.h"
+
